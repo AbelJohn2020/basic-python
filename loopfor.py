df303b53856b6f1702dfa43e969411f1e383b455
Abel AR <abel_john_14@hotmail.com>
2021-05-19 19:09:49 -0500

    Enter to for loop

---
+++ loopfor.py
@@ -0,0 +1,10 @@
+def run():
+    # i=0
+    # while i <= 1000:
+    #     print(i)
+    #     i += 1
+    for i in range(11):         #for i in the range (0 to 11)
+        print(2**i)
+
+if __name__ == '__main__':
+    run()
